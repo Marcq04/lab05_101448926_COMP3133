b70d13c03e2ab5e091ebe63c19b7fafa16fe3176
Marcq04 <156446124+Marcq04@users.noreply.github.com>
2025-02-13 13:49:33 -0500

    updated resolvers to handle bulk insertion

--- resolvers.js
+++ resolvers.js
@@ -5,20 +5,22 @@ const resolvers = {
     getAllMovies: async () => await Movie.find(),
     getMovieById: async (_, { id }) => await Movie.findById(id),
   },
-  
   Mutation: {
     addMovie: async (_, { name, director_name, production_house, release_date, rating }) => {
-      const newMovie = new Movie({ name, director_name, production_house, release_date, rating });
-      return await newMovie.save();
+      const movie = new Movie({ name, director_name, production_house, release_date, rating });
+      await movie.save();
+      return movie;
     },
-    
+    addMoviesBulk: async (_, { movies }) => {
+      const result = await Movie.insertMany(movies);
+      return result;
+    },
     updateMovie: async (_, { id, name, director_name, production_house, release_date, rating }) => {
       return await Movie.findByIdAndUpdate(id, { name, director_name, production_house, release_date, rating }, { new: true });
     },
-    
     deleteMovie: async (_, { id }) => {
       await Movie.findByIdAndDelete(id);
-      return "Movie deleted successfully";
+      return "Movie deleted successfully!";
     }
   }
 };
