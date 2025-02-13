6d01258ef985fd76484a688dd33353ea15f60a74
Marcq04 <156446124+Marcq04@users.noreply.github.com>
2025-02-13 13:00:49 -0500

    updated code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-//import ApolloServer
-
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 
 //Store sensitive information to env variables
 const dotenv = require('dotenv');
@@ -13,22 +13,21 @@ const mongodb_atlas_url = process.env.MONGODB_URL;
 
 //TODO - Replace you Connection String here
 const connectDB = async() => {
-    try{
-      mongoose.connect(mongodb_atlas_url, {
+    try {
+      await mongoose.connect(mongodb_atlas_url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-      }).then(success => {
-        console.log('Success Mongodb connection')
-      }).catch(err => {
-        console.log('Error Mongodb connection')
       });
-    } catch(error) {
-        console.log(`Unable to connect to DB : ${error.message}`);
-      }
+      console.log('Success Mongodb connection');
+    } catch (error) {
+      console.log(`Unable to connect to DB : ${error.message}`);
+    }
   }
 
 //Define Apollo Server
-
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const server = new ApolloServer({ typeDefs, resolvers });
 
 //Define Express Server
 const app = express();
@@ -36,10 +35,11 @@ app.use(express.json());
 app.use('*', cors());
 
 //Add Express app as middleware to Apollo Server
-
+server.applyMiddleware({ app, cors: false });
 
 //Start listen 
 app.listen({ port: process.env.PORT }, () => {  
   console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
   connectDB()
 });
+
